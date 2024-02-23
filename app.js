2a3bb1bfc7d87bffad7a43f1bd70a9aa4b7a0a41
cheyma2001 <65093603+MedAZIZI@users.noreply.github.com>
2024-02-23 17:53:15 +0100

    init 3

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.json({
+    message: 'Hello, world!',
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App is listening on port ${port}`)
 })
-
