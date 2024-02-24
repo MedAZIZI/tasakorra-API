ab4b05306c37a1ee8a8605665d326b6bf64ffcb2
cheyma2001 <65093603+MedAZIZI@users.noreply.github.com>
2024-02-24 23:57:35 +0100

    offre

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require('express');
+const { Pool } = require('pg');
 
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Configuration de la connexion à la base de données
+const pool = new Pool({
+  user: 'koyeb-adm',
+  host: 'ep-rapid-mud-a2jsn9m8.eu-central-1.pg.koyeb.app',
+  database: 'koyebdb',
+  password: 'ENnY1QXI9cmr',
+  port: 5432, // Port par défaut de PostgreSQL
+});
 app.get('/', (req, res) => {
   res.json({
     Title: 'Tasakorra',
@@ -9,7 +19,19 @@ app.get('/', (req, res) => {
     Date: Date.now(),
   })
 })
+// Définition de la route /offre pour récupérer les données de la table "offres"
+app.get('/offre', async (req, res) => {
+  try {
+    // Exécutez une requête SQL pour sélectionner toutes les offres
+    const { rows } = await pool.query('SELECT * FROM offres');
+    res.json(rows); // Envoyer les données récupérées en tant que réponse JSON
+  } catch (error) {
+    console.error('Erreur lors de la récupération des données:', error);
+    res.status(500).json({ message: 'Erreur lors de la récupération des données' });
+  }
+});
 
+// Démarrage du serveur
 app.listen(port, () => {
-  console.log(`App is listening on port ${port}`)
-})
+  console.log(`App écoute sur le port ${port}`);
+});
